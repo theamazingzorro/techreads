c279d042b0256a375416356f7e101e86dfb38ef0
Fox Noland <fnoland@manifestcorp.com>
2021-11-05 16:45:31 -0400

    Initial project generated by start.spring.io with Spring Web

---
+++ src/test/java/com/manifestcorp/techreads/TechreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manifestcorp.techreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
